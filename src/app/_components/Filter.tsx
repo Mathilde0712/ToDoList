433789f514caac5ee07ed2860ddcacea1f107e2c
Mathilde Besse <mathildebesse07@gmail.com>
2024-10-17 16:28:22 +0200

    amélioration style formulaire

--- src/app/_components/Filter.tsx
+++ src/app/_components/Filter.tsx
@@ -1,29 +1,51 @@
 export const Filter = () => {
   return (
     <div className="flex-[2] w-full mb-1 pl-1 text-secondary rounded-xl border border-secondary">
-      <h2>Filtrer</h2>
+      <h2 className="m-2">Filtrer</h2>
       <ul className="flex-col max-md:flex max-md:flex-row gap-2">
         <li>
           <label>
-            <input type="checkbox" id="all" name="all" />
+            <input
+              className="m-3 max-md:m-1"
+              type="checkbox"
+              id="all"
+              name="all"
+            />
             Toutes
           </label>
         </li>
         <li>
           <label>
-            <input type="checkbox" id="en-cours" name="En cours" />
+            <input
+              className="m-3 max-md:m-1"
+              type="checkbox"
+              id="en-cours"
+              name="En cours"
+            />
             En cours
           </label>
         </li>
+      </ul>
+      <ul className="flex-col max-md:flex max-md:flex-row gap-2">
         <li>
           <label>
-            <input type="checkbox" id="date" name="date" />
-            Par date d'échéance
+            <input
+              className="m-3 max-md:m-1"
+              type="checkbox"
+              id="date"
+              name="date"
+            />
+            Par date d&apos;échéance
           </label>
         </li>
         <li>
           <label>
-            <input type="checkbox" id="fini" name="fini" />
+            <input
+              className="m-3 max-md:m-1"
+              type="checkbox"
+              id="fini"
+              name="fini"
+            />
             Terminées
           </label>
         </li>
--- src/app/_components/Form.tsx
+++ src/app/_components/Form.tsx
@@ -1,36 +1,49 @@
 export const Form = () => {
   return (
-    <div className="flex-[3] w-full mb-1 pl-1 text-secondary rounded-xl border border-secondary shadow ">
-      <h2>Ajouter une tâche</h2>
-      <form>
-        <ul>
+    <div className="flex-[3] w-full mb-1 pl-1 text-secondary rounded-xl border border-secondary shadow items-center ">
+      <h2 className="m-2">Ajouter une tâche</h2>
+      <form className="flex flex-col ">
+        <ul className="flex flex-col">
           <li>
             <label>
-              Titre :
-              <input type="text" id="titre" name="titre" />
-            </label>
-          </li>
-          <li>
-            <label>
-              Description :
-              <textarea id="description" name="description" />
-            </label>
-          </li>
-          <li>
-            <label>
-              Date d'échéance :
               <input
+                className="border border-secondary rounded-md m-2 w-9/12 p-2 "
+                type="text"
+                id="titre"
+                name="titre"
+                placeholder="Titre"
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                className="border border-secondary rounded-md m-2 w-9/12 p-2"
+                id="description"
+                name="description"
+                placeholder="Description"
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                className="border border-secondary rounded-md m-2 w-9/12 p-2 "
                 type="date"
                 id="date"
                 name="date"
-                min="2024-10-09"
                 max="2030-12-31"
               />
             </label>
           </li>
         </ul>
         <div className="flex">
-          <button type="submit">Valider</button>
+          <button
+            className="border border-secondary rounded-md m-2 w-9/12 p-2"
+            type="submit"
+          >
+            Valider
+          </button>
         </div>
       </form>
     </div>
